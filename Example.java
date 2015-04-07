2e1be5ced0ce430e8f0d218cfa2a8703f05b51a1
aamarjl <amarnath@pillar4.com>
2015-04-07 12:49:57 +0530

    committed

---
+++ Example.java
@@ -0,0 +1,14 @@
+import java.util.Stack;
+
+
+public class Example {
+
+	public static void main(String[] args) {
+		
+		Stack<Integer> s=new Stack<Integer>();
+		s.add(20);
+		int a=(int)s.peek();
+		System.out.println(a);
+	}
+
+}
